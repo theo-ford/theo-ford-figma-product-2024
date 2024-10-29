6fb5f0b5af00e89ed220e7a570989a4f7ff6ad8e
Theo Ford <theoford@theos-mbp.mynetworksettings.com>
2024-10-29 15:33:12 -0400

    testing mobile index

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,24 +26,28 @@ import { NavGrid } from "../components/tf/nav-grid/nav";
 const GlobalStyle = createGlobalStyle`
   html {
     background-color: black;
-    overflow-x: clip;
-    max-width: 100vw;
   }
   body {
-    // https://stackoverflow.com/questions/47095596/body-overflow-x-hidden-breaks-position-sticky
     background-color: black;
     overflow-x: clip;
     width: 100vw;
     height: 100vh;
     overflow: hidden;
-
   }
   p {
     letter-spacing: -0.2px;
   }
+  /* @media not all and (hover: none) {
+  } */
+  @media (hover:none) {
+    body {
+      background: black;
+      max-width: 100vw;
+      /* height: 200vh; */
+      overflow: scroll;
+    }
+  }
 `;
-
-/* BOTH */
 
 /* DESKTOP */
 
@@ -53,30 +57,6 @@ const DesktopNavP = styled.p`
   a.selected {
     color: white;
   }
-  @media (max-width: 666px) {
-    display: none;
-  }
-`;
-
-/* MOBILE */
-const MobileLeftCol = styled.div`
-  grid-column: span 2;
-  mix-blend-mode: exclusion;
-`;
-const MobileRightCol = styled.div`
-  grid-column: span 2;
-  mix-blend-mode: exclusion;
-`;
-const MobileNavP = styled.p`
-  display: none;
-  color: #878787;
-  mix-blend-mode: exclusion;
-  &.selected {
-    color: white;
-  }
-  @media (max-width: 666px) {
-    display: block;
-  }
 `;
 
 /* - - - - - - - top level container */
@@ -84,6 +64,12 @@ const MenuCon = styled.div`
   position: fixed;
   top: 12.5px;
   left: calc(50vw + 12px);
+  z-index: 50000;
+  @media (max-width: 666px) {
+    left: 10px;
+    width: 100%;
+    top: 10px;
+  }
 `;
 
 const ProjectsCon = styled.div`
@@ -95,6 +81,9 @@ const ProjectsCon = styled.div`
   /* height: 100vh;
   width: 100vw; */
   bottom: 12.5px;
+  @media (max-width: 666px) {
+    position: relative;
+  }
 `;
 
 const ImgConConCon = styled.div`
@@ -125,6 +114,9 @@ const TableCon = styled.div`
   z-index: 3000;
   margin-top: 20px;
   margin-bottom: 20px;
+  @media (max-width: 666px) {
+    margin-top: 200px;
+  }
 `;
 const Grid16 = styled.div`
   display: grid;
@@ -139,6 +131,7 @@ const Grid16 = styled.div`
     width: calc(100% - 20px);
     margin-left: 10px;
     grid-gap: 10px;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
   }
 `;
 const IndexImg = styled.div`
@@ -153,6 +146,10 @@ const IndexBodyP = styled.p`
   white-space: nowrap;
   overflow: hidden;
   letter-spacing: -0.2px;
+
+  /* @media (hover) {
+    color: red;
+  } */
 `;
 const ProjectCon = styled.div`
   z-index: 3000;
@@ -176,10 +173,18 @@ const ProjectCon = styled.div`
   } */
   
     // ${({ activeProject }) => activeProject && console.log(activeProject)}
-
+    
+    @media (hover: none) {
+      opacity: 1;
+      /* height: 500px; */
+      margin-top: 200px;
+    }
 `;
 const InformationCon = styled.div`
   height: 20px;
+  @media (hover: none) {
+    height: auto;
+  }
 `;
 const ImgSpacer = styled.div`
   grid-column: span 8;
@@ -187,18 +192,38 @@ const ImgSpacer = styled.div`
     display: none;
   }
 `;
+const MobileIndexNumberCon = styled.div`
+  display: none;
+  p {
+    font-size: 50px;
+    color: white;
+    font-family: "HelveticaNowDisplay";
+    font-weight: bold;
+  }
+  margin-bottom: -5px;
+  @media (hover: none) {
+    grid-column: span 4;
+    display: block;
+  }
+`;
+const MobileProjectImgCon = styled.div`
+  display: none;
+  grid-column: span 4;
+  @media (hover: none) {
+    display: block;
+  }
+`;
 const ProjectTitleCon = styled.div`
   grid-column: 9 / span 2;
   /* background-color: red; */
   @media (max-width: 666px) {
-    grid-column: span 8;
+    grid-column: 1 / span 2;
   }
 `;
 const CategoryCon = styled.div`
   grid-column: 11 / span 3;
-  /* background-color: blue; */
-  @media (max-width: 666px) {
-    grid-column: span 8;
+  @media (hover: none) {
+    display: none;
   }
 `;
 const LocationCon = styled.div`
@@ -206,18 +231,24 @@ const LocationCon = styled.div`
   margin-top: -15px;
   /* background-color: green; */
   @media (max-width: 666px) {
-    display: none;
+    grid-column: 3 / span 2;
+    margin-top: 0px;
   }
 `;
 const YearCon = styled.div`
   /* background-color: yellow; */
   grid-column: 16 / span 1;
   @media (max-width: 666px) {
-    display: none;
+    grid-column: 3 / span 2;
+    margin-top: -10px;
   }
 `;
 
-const TableHeaderCon = styled.div``;
+const TableHeaderCon = styled.div`
+  @media (hover: none) {
+    display: none;
+  }
+`;
 
 const IndexTitleP = styled.p`
   font-size: 12px;
@@ -245,6 +276,22 @@ const PageTitleCon = styled.div`
   div {
     display: block;
   }
+  @media (max-width: 666px) {
+    margin-top: 200px;
+    position: relative;
+    svg.theo_ford {
+      height: 35px;
+      margin-left: -108px;
+    }
+    svg.s {
+      height: 35px;
+      margin-left: -119px;
+    }
+    svg.portfolio_for_figma {
+      height: 45px;
+      margin-left: -185px;
+    }
+  }
 `;
 const NumCon = styled.div`
   p {
@@ -256,36 +303,10 @@ const NumCon = styled.div`
   position: fixed;
   bottom: 12.5px;
   left: 12.5px;
-`;
-
-const handlePosition = columnProp => {
-  switch (columnProp) {
-    case 1:
-      return "1";
-    case 2:
-      return "2";
-    case 3:
-      return "3";
-    case 4:
-      return "4";
-    case 5:
-      return "5";
-    case 6:
-      return "6";
-    case 7:
-      return "7";
-    case 8:
-      return "8";
-    case 9:
-      return "9";
-    case 10:
-      return "10";
-    case 11:
-      return "11";
-    case 12:
-      return "12";
+  @media (hover: none) {
+    display: none;
   }
-};
+`;
 
 const ImgCon = styled.div`
   /* grid-column: span 4; */
@@ -334,6 +355,9 @@ const ImgCon = styled.div`
     height: 100%;
     object-fit: cover;
   }
+  @media (hover: none) {
+    display: none;
+  }
 `;
 
 const SlideShowCon = styled.div``;
@@ -362,52 +386,22 @@ const ProjectIndex = ({ data }) => {
       }
     }, [setCurrentPage]);
 
-    if (isPageWide) {
-      return (
-        <MenuCon>
-          <DesktopNavP>
-            <Link to="/" className="selected">
-              Index,{" "}
-            </Link>
-            <Link to="/resume">Resume, </Link>
-            <Link
-              to="/about"
-              className={currentPage == "about" ? "selected" : ""}
-            >
-              About
-            </Link>
-          </DesktopNavP>
-        </MenuCon>
-      );
-    }
-    if (!isPageWide) {
-      return (
-        <>
-          <MobileLeftCol>
-            <Link to="">
-              <MobileNavP
-                className={currentPage == "project_index" ? "selected" : ""}
-              >
-                Index
-              </MobileNavP>
-            </Link>
-          </MobileLeftCol>
-
-          <MobileRightCol>
-            <Link to="/about">
-              <MobileNavP className={currentPage == "about" ? "selected" : ""}>
-                About
-              </MobileNavP>
-            </Link>
-            <MobileNavP>
-              <Link target="_blank" to="https://www.instagram.com/tf.public/">
-                <span style={{ marginLeft: "0px" }}>Instagram</span>
-              </Link>
-            </MobileNavP>
-          </MobileRightCol>
-        </>
-      );
-    }
+    return (
+      <MenuCon>
+        <DesktopNavP>
+          <Link to="/" className="selected">
+            Index,{" "}
+          </Link>
+          <Link to="/resume">Resume, </Link>
+          <Link
+            to="/about"
+            className={currentPage == "about" ? "selected" : ""}
+          >
+            About
+          </Link>
+        </DesktopNavP>
+      </MenuCon>
+    );
   };
   const projectIndexSelectArray = data.prismicProjectIndexSelect.data.project_relationship_group.map(
     (content, index) => {
@@ -465,8 +459,6 @@ const ProjectIndex = ({ data }) => {
         content.content.project_relationship_field.document.data
           .index_preview_img
       );
-      // console.log(index);
-      // console.log(activeIndex);
       return (
         <>
           <Link
@@ -475,6 +467,12 @@ const ProjectIndex = ({ data }) => {
             <ProjectCon activeProject={activeIndex} projectIndex={index}>
               <InformationCon>
                 <Grid16>
+                  <MobileIndexNumberCon>
+                    <p>{index + 1}</p>
+                  </MobileIndexNumberCon>
+                  <MobileProjectImgCon>
+                    <GatsbyImage image={index_image} />
+                  </MobileProjectImgCon>
                   <ProjectTitleCon
                     onMouseEnter={e => activeIndexFunction(e, index)}
                   >
@@ -499,6 +497,14 @@ const ProjectIndex = ({ data }) => {
                       )}
                     </IndexBodyP>
                   </CategoryCon>
+                  <LocationCon>
+                    <IndexBodyP>
+                      {
+                        content.content.project_relationship_field.document.data
+                          .location.text
+                      }
+                    </IndexBodyP>
+                  </LocationCon>
                   <YearCon>
                     <IndexBodyP>
                       {" "}
@@ -508,14 +514,6 @@ const ProjectIndex = ({ data }) => {
                       }
                     </IndexBodyP>
                   </YearCon>
-                  <LocationCon>
-                    <IndexBodyP>
-                      {
-                        content.content.project_relationship_field.document.data
-                          .location.text
-                      }
-                    </IndexBodyP>
-                  </LocationCon>
                 </Grid16>
               </InformationCon>
             </ProjectCon>
@@ -569,13 +567,14 @@ const ProjectIndex = ({ data }) => {
       </Helmet>
 
       <Nav></Nav>
+
       <PageTitleCon>
         <div>
-          <TheoFord />
-          <S></S>
+          <TheoFord className="theo_ford" />
+          <S className="s"></S>
         </div>
         <div>
-          <PortfolioForFigma />
+          <PortfolioForFigma className="portfolio_for_figma" />
         </div>
       </PageTitleCon>
 
